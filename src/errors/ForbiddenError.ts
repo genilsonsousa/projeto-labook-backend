553569febdc5a94c1c7f208e34e9958c4e74a594
genilsonsousa <108639542+genilsonsousa@users.noreply.github.com>
2023-06-06 19:03:44 -0300

    Revert "projeto enviado"

--- src/errors/ForbiddenError.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from "./BaseError";
-
-export class ForbiddenError extends BaseError {
-    constructor(
-        message: string = "Token válido, mas sem permissões suficientes"
-    ) {
-        super(403, message)
-    }
-}

3ae71902651c7ccc4ee1e02eee53e68457a45c9d
YOUR NAME <YOUR@EMAIL.com>
2023-06-06 18:55:57 -0300

    projeto enviado

---
+++ src/dto/Post/editPostById.dto.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export interface EditPostByIdInputDTO {
+  idToEditPost: string;
+  content: string;
+  token: string;
+}
+
+export interface EditPostByIdOutputDTO {
+  message: string;
+}
+
+export const EditPostByIdSchema = z
+  .object({
+    idToEditPost: z.string().min(1),
+    content: z.string().min(1),
+    token: z.string().min(1),
+  })
+  .transform((data) => data as EditPostByIdInputDTO);
